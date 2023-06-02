32f5474e221a6ffec878a6447c81ee43e7ffff61
Fatima Bouyarmane <116899946+FatimaBouyarmane@users.noreply.github.com>
2023-06-02 13:49:56 +0100

    Update project_coffre_fort.ino

--- project_coffre_fort.ino
+++ project_coffre_fort.ino
@@ -4,7 +4,7 @@
 #include <Keypad.h>
  
 #define SS_PIN 53
-#define RST_PIN 5
+#define RST_PIN 50
 #define SERVO_PIN 3
 #define MAX_ATTEMPTS 3
 
@@ -17,17 +17,17 @@ Servo myservo;
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 
-
-
  int R=6;
  int V=7;
  int buzzer=4;
  int wrongAttempts=0;
  int rightAttempts=0;
- int wrongpsw=0;  
+ int wrongpsw=0;
+ char tab[3];  
 //Pad
-char* password = "888"; // change the password here, just pick any 3 numbers
+char* password = "123A"; // change the password here, just pick any 3 numbers
 int position = 0;
+int error = 0;
 const byte ROWS = 4;
 const byte COLS = 4;
 char keys[ROWS][COLS] = {
@@ -39,8 +39,7 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = { 21, 20, 19, 18 };
 byte colPins[COLS] = { 17, 16, 15, 14 };
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-int RedpinLock = 12;
-int GreenpinUnlock = 13;
+
 
 
 
@@ -50,7 +49,7 @@ void setup()
   SPI.begin();          // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
   myservo.attach(SERVO_PIN);
-  Serial.println("Put your card to the reader...");
+  Serial.println("Put your card to the reader Or enter your password");
   Serial.println();
   pinMode(V,OUTPUT);
   pinMode(R,OUTPUT);
@@ -60,13 +59,13 @@ void setup()
 }
 
 
-
+ 
 
 void loop() {
 
-
-
   char key = keypad.getKey();
+  if( key!=NO_KEY)
+  {
    if (key == '*' || key == '#') 
    {
       position = 0;
@@ -76,11 +75,29 @@ void loop() {
    {
       position ++;
    }
-   if (position == 3)
+   
+   else  
    {
-      LockedPosition(false);
+     position++;
+     error++;     
+   
    }
 
+
+  
+   if (position == 4 && error == 0)
+   {
+      LockedPosition(false);
+      position = 0;  
+     
+   }
+    
+else if(position == 4 && error != 0){  
+    IncorrectKey();
+    position = 0;     
+    error=0;
+  } 
+  }
   delay(100);
 
   
@@ -131,11 +148,11 @@ void UnlockDoor(){
     Serial.println("Access autorise");
     digitalWrite(V,HIGH);
     Serial.println();
-    delay(3500);
+    delay(500);
     digitalWrite(V,LOW);
     myservo.write( 0 );
     Serial.println("the door is opened");
-    delay(3000);
+    delay(500);
     if (rightAttempts % 2==0)
     {
     myservo.write( 180 );
@@ -152,14 +169,14 @@ void UnlockDoor(){
     for(int i=0;i<MAX_ATTEMPTS;i++)
     {
          digitalWrite(R,HIGH);
-         delay(500);
+         delay(400);
          digitalWrite(R,LOW);
          if (wrongAttempts>= MAX_ATTEMPTS)
          {
                         
             digitalWrite(R,HIGH)  ;                    
             digitalWrite(buzzer,LOW);//l3aks
-            delay(5000);
+            delay(3000);
             digitalWrite(buzzer,HIGH);
             digitalWrite(R,LOW);
             wrongAttempts=0;
@@ -174,11 +191,11 @@ void UnlockDoor(){
 void LockedPosition(int locked){
     if (locked)
     {
-       digitalWrite(RedpinLock, HIGH);
-       digitalWrite(GreenpinUnlock, LOW);
-       myservo.write(180);
-       digitalWrite(V,HIGH);
-       digitalWrite(buzzer,LOW);
+      // digitalWrite(RedpinLock, HIGH);
+       //digitalWrite(GreenpinUnlock, LOW);
+      myservo.write(180);
+      digitalWrite(V,HIGH);
+      digitalWrite(buzzer,LOW);
       delay(300);   
       digitalWrite(buzzer,HIGH);  
       digitalWrite(V,LOW); 
@@ -187,9 +204,9 @@ void LockedPosition(int locked){
     else
     {
              
-      if(password="888"){          
-      digitalWrite(RedpinLock, LOW);
-      digitalWrite(GreenpinUnlock, HIGH);
+      if(password=="123A"){          
+      //digitalWrite(RedpinLock, LOW);
+      //digitalWrite(GreenpinUnlock, HIGH);
       myservo.write(0);
       digitalWrite(V,HIGH);
       digitalWrite(buzzer,LOW);      
@@ -199,32 +216,31 @@ void LockedPosition(int locked){
       }
       else {
         LockDoor();
-        }
+        IncorrectKey ();       }
       
     }
 
  }
 
-/*
-void TestPassword(){
-   if (password=="888") {
-      LockedPosition(false);  
-   }
+void IncorrectKey() {
+  wrongpsw++;
+  digitalWrite(R, HIGH); // Turn on the red LED
+  delay(400);
+  if(wrongpsw>=3) 
+  { 
+  while(true)
+  {       
 
-   else {
-      wrongpsw++;      
-      digitalWrite(R,HIGH); 
-      if (wrongpsw >= 3)
-      {
-         digitalWrite(buzzer,LOW);
-         delay(3000);
-         digitalWrite(buzzer,HIGH); 
-      } 
-      digitalWrite(R,LOW);          
-    }     
-   
+  digitalWrite(buzzer, LOW); // Activate the buzzer
+  delay(3000); // Delay for 1 second
+  digitalWrite(buzzer, HIGH); // Deactivate the buzzer
+  wrongpsw=0;
+  break;  
+  } 
+  }
+  digitalWrite(R, LOW); // Turn off the red LED
+  
+  
 }
-
-*/
 
 
